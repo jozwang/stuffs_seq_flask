408bfc298a8534b09f0c209466993120824cbc3e
jozwang <9334262+jozwang@users.noreply.github.com>
2025-07-26 17:21:25 +1000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,14 @@
+# This file contains the WSGI configuration required to serve up your
+# web application at http://<your-username>.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI app object.
 
+import sys
+
+# Add your project's directory to the Python path.
+# Replace '<your-username>' and '<your-repo-name>' with your actual details.
+project_home = '/home/<your-username>/<your-repo-name>' 
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
+
+# Import the 'app' object from your flask_app.py file
+from flask_app import app as application
